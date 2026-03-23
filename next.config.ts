84c1680d91ea706f4e974501bac78bacb01a06d8
LeeSeungYong <du58ck@gmail.com>
2026-03-23 20:13:46 +0900

    빌드 에러 재수정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    'ckeditor5',
+    '@ckeditor/ckeditor5-react',
+    'grapesjs',
+    '@grapesjs/react',
+    'grapesjs-preset-webpage',
+  ],
   async rewrites() {
     return [
       {
