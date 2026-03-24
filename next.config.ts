4a4f39837c89910911152c3aa06080ae0ce1dab8
jhw <jhw811@gmail.com>
2026-03-24 10:43:47 +0900

    next config api 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
+const API_BACKEND = process.env.API_URL || 'http://api-service:8000'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_BACKEND}/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
